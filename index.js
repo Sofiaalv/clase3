0beb98db6ff89b2f834f42c3727d6f30875e8593
Sofia Alvarez <sofia.alvarez92@hotmail.com>
2021-09-29 16:48:06 -0300

    espacios2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ for( let i = 0; i <10 ; i++){
     let nombreUsuario = prompt("Ingresar nombre");
     let votoAb = prompt("Ingresar A o B");
 
-    if( votoAb == " A " || votoAb == " a "){
+    if( votoAb == "A" || votoAb == "a"){
         console.log(" Usuario " + nombreUsuario + " Voto A ");
     }
-    else if( votoAb ==" B " || votoAb == " b "){
+    else if( votoAb =="B" || votoAb == "b"){
         console.log(" Usuario " + nombreUsuario + " Voto B ");
     }
     
